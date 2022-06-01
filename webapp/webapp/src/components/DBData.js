db58be809c8db2574cfdc2fe55d74498f8dcd7ec
nkusurin <nk51342@fer.hr>
2022-06-01 20:44:13 +0200

    Fetched data from wx_data

--- webapp/webapp/src/components/DBData.js
+++ webapp/webapp/src/components/DBData.js
@@ -3,48 +3,32 @@ import ReactDOM from 'react-dom';
 
 // STARTS HERE
 import * as AWS from 'aws-sdk';
-import { ConfigurationOptions } from 'aws-sdk';
+import { ConfigurationOptions, credentials } from 'aws-sdk';
 import {fetchData} from './AWSFunctions';
 
+const fetchDataFromDynamoDb = () => {
 
-
-
-const fetchDataFormDynamoDb = () => {
-    const configuration: ConfigurationOptions = {
-        region: 'eu-central-1',
-        endpoint: 'http://a2mr3kdz48c7g8-ats.iot.eu-central-1.amazonaws.com',
+    AWS.config.update({
+        IdentityPoolId: 'eu-central-1:9426c50a-6fa8-4bbc-951a-c09c395122cd',
+        region: "eu-central-1",
+        accessKeyId: '0nQwzBfv9jV6nwgd5lReWgsdqiz98xWLIf99ynTE',
         secretAccessKey: 'AKIATT53DCKUKE73QKVO',
-        accessKeyId: '0nQwzBfv9jV6nwgd5lReWgsdqiz98xWLIf99ynTE'
-    }
+    });
 
-    AWS.config.update(configuration);
-    console.log("Trying to fetch data from database table...")
-    fetchData('wx_data')
+    console.log("Trying to fetch data from database table..." );
+    fetchData('wx_data');
 }
 
-
 const DBData = () => {
-
-
 
     return (
         <div className="dbdata">
 
             <div className="message">Helooo!</div>
-           <button onClick= {fetchDataFormDynamoDb}>Fetch data</button>
+           <button onClick= {fetchDataFromDynamoDb}>Fetch data</button>
 
         </div>
     );
-
-
-
-
-
-
-
-
-
-
 };
 
 export default DBData;
